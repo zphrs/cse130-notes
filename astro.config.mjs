6eb594b77fe6e687a98473620242d29f3a8d017b
zphyrj <46908127+zphyrj@users.noreply.github.com>
2024-01-16 15:48:10 -0800

    Updates

--- astro.config.mjs
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "astro/config"
-
-// https://astro.build/config
-export default defineConfig({
-  trailingSlash: "always",
-  site: "https://zphrs.github.io",
-  base: "/cse130-notes",
-})
